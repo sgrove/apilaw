6ffac50891103908750a2c6b7b1f62e774366f1d
Sean Grove <s@bushi.do>
2017-12-31 05:29:24 -0800

    ..

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 // Import typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
+// import 'typeface-montserrat'
+// import 'typeface-merriweather'
 
 import profilePic from './profile-pic.jpg'
 
@@ -12,17 +12,12 @@ class Bio extends React.Component {
       <div
         style={{
           display: 'flex',
-          marginBottom: rhythm(2.5),
         }}
-      >
+        >
         <img
           src={profilePic}
           alt={`Kyle Mathews`}
           style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
           }}
         />
         <p>
